0cc0c53ad569be6778d45f8cb6ac83e82524cd43
Matt Mills <mattmillsxyz@gmail.com>
2020-07-16 10:32:07 -0500

    Add logo.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Hero = props => {
+const Hero = (props) => {
   return (
     <section className="row">
       <div className="col-md-8 offset-md-2">
+        <img
+          src="/img/braille-generator.jpg"
+          width="315"
+          alt="Braille Generator"
+        />
         <h1 style={{ fontWeight: 'bold' }}>Braille Generator</h1>
         <p>
           Simply type in a word or phrase to have it converted to braille. Click
