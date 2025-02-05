1e2f4f716b210770d287363896e9bc9e286d97e9
Matt Mills <mattmillsxyz@gmail.com>
2025-02-05 17:45:06 -0600

    chore: move to vite and modernize packages

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+    commonjsOptions: {
+      include: [/braille/],
+    },
+  },
+  publicDir: "public",
+  optimizeDeps: {
+    include: ["braille"],
+  },
+});
