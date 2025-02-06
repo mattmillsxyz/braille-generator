fd7690ab97d3bd9df565bc1cc7134a0c05f793de
Matt Mills <mattmillsxyz@gmail.com>
2025-02-05 20:39:03 -0600

    chore: remove old files

--- src/types/braille.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "braille" {
-  export function toBraille(text: string): string;
-  export function toText(braille: string): string;
-}
--- src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export const sanitizeFileName = (text: string): string => {
-  return text
-    .replace(/[^a-zA-Z0-9\s-]/g, "")
-    .replace(/\s+/g, "-")
-    .toLowerCase();
-};
